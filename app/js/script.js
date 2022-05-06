ffc1aedea1fd1fb1691659945b8d570e2e414e5e
Vipul Vaishnav <007vipulrank1@gmail.com>
2022-05-06 16:57:56 +0530

    Update Readme

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const prev = document.querySelector('.sliders__btn--prev');
+const next = document.querySelector('.sliders__btn--next');
+
+const slideOne = document.querySelector('.main__slide-box__slide--1');
+const slideTwo = document.querySelector('.main__slide-box__slide--2');
+
+prev.addEventListener('click', () => {
+  if (slideOne.classList.contains('hide')) {
+    slideOne.classList.remove('hide');
+    slideTwo.classList.add('hide');
+  }
+});
+
+next.addEventListener('click', () => {
+  if (slideTwo.classList.contains('hide')) {
+    slideTwo.classList.remove('hide');
+    slideOne.classList.add('hide');
+  }
+});
